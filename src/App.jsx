9893ee4068893772ba03c027ff24501c85214e13
sanjanashreem <sanjanashree64@gmail.com>
2026-03-08 11:26:49 +0530

    changes in about

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //import { Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+//import Contact from "./pages/Blog";
+//<Route path="/blog" element={<Blog />} />
 import GirlFinds from "./pages/GirlFinds";
 import Beauty from "./pages/Beauty";
 import Skincare from "./pages/Skincare";
@@ -13,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/girly_finds" element={<GirlFinds />} />
         <Route path="/beauty" element={<Beauty />} />
         <Route path="/skincare" element={<Skincare />} />
